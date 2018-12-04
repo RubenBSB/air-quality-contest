e558e3cc26590907ec2c15a6b6dd37b8285e7b06
Ruben Bousbib <rbousbib@gmail.com>
2018-12-04 21:51:38 +0100

    Building model.

--- learn.py
+++ learn.py
@@ -1,10 +1,41 @@
 from keras.layers import LSTM, Dense
 from keras.models import Sequential
+from data.data_preprocess import df_bretagne_horaire_pm10
+import numpy as np
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (10, 6)
 
 model = Sequential()
-model.add(LSTM(64, input_shape=(None, 500)))
-model.add(Dense(1, activation='relu'))
+model.add(LSTM(5, input_shape = (30,1)))
+model.add(Dense(1, activation='linear'))
 model.compile(optimizer='adam', loss='mse')
 
+df_bretagne_horaire_pm10
+
+val = df_bretagne_horaire_pm10['Brest Mace'].values
+m = min(val)
+M = max(val)
+val2 = (val-m)/(M-m)
+
+def create_dataset(values, look_back):
+    X = []
+    Y = []
+    for k in range(len(values)-look_back+1):
+        x = values[k:k+look_back]
+        if k+look_back != len(values):
+            y = values[k+look_back]
+        else:
+            y = -1
+        X.append(x)
+        Y.append(y)
+    X = np.array(X)
+    Y = np.array(Y)
+    return X,Y
+
+X,Y = create_dataset(val2,30)
+
+X = X.reshape(X.shape[0], X.shape[1], 1)
 
 
